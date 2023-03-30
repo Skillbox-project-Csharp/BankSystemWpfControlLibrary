13d7b2a34685438005e6014f6d5e39013d06a4fd
Ornetolog <33785981+Ornetolog@users.noreply.github.com>
2023-03-30 13:09:47 +0300

    Добавьте файлы проекта.

---
+++ SelectionWindows/BankAccountSelectionWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using BankSystemLibrary.BankSystem.BankAccounts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BankSystemWpfControlLibrary.SelectionWindows
+{
+
+    /// <summary>
+    /// Логика взаимодействия для OpenAccountWindow.xaml
+    /// </summary>
+    public partial class BankAccountSelectionWindow : Window
+    {
+        public BankAccount GetBankAccount
+        {
+            get
+            {
+                switch (TypesBankAccounts.SelectedIndex)
+                {
+                    case 0: return new BankAccount();
+                    case 1: return new NoDepositAccount();
+                    case 2: return new DepositAccount();
+                    default: return null;
+                }
+
+            }
+        }
+        public BankAccountSelectionWindow()
+        {
+            InitializeComponent();
+        }
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
---
+++ SelectionWindows/EmployeeSelectionWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using BankSystemLibrary.BankWorkers;
+using BankSystemLibrary;
+
+namespace BankSystemWpfControlLibrary.SelectionWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class EmployeeSelectionWindow : Window
+    {
+        public EmployeeSelectionWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Worker GetWorker
+        {
+            get
+            {
+                switch (TypesWorker.SelectedIndex)
+                {
+                    case 0: return new Manager(InputName.Text, InputSurName.Text, InputPatronymic.Text);
+                    case 1: return new Consultant(InputName.Text, InputSurName.Text, InputPatronymic.Text);
+                    default: return null;
+                }
+
+            }
+        }
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
